e5282452eaee009b8e5ecf84e890aeb7d78bac21
Logan King <seanloganking@gmail.com>
2022-06-08 11:13:42 -0400

    test shrinking image

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use colored::Colorize;
 use glob::{glob, GlobError};
-use image::{GenericImageView, Rgb, RgbImage, Rgba, RgbaImage};
-use std::{fs, path::PathBuf};
+use image::{imageops::FilterType, DynamicImage, GenericImageView, RgbaImage};
+use std::path::PathBuf;
 
 /// Returns a list of all files in a directory and it's subdirectories
 pub fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobError> {
@@ -45,7 +45,7 @@ struct Bounds {
 }
 
 struct FilenameAndNumbers {
-    fileName: PathBuf,
+    file_name: PathBuf,
     x: i32,
     z: i32,
 }
@@ -53,7 +53,7 @@ struct FilenameAndNumbers {
 fn main() {
     let files = get_files_in_dir("./input", "").unwrap();
 
-    if files.len() == 0 {
+    if files.is_empty() {
         print_err("no files found");
         return;
     }
@@ -69,16 +69,14 @@ fn main() {
 
     // find max and min dimensions
     for file in &files {
-        // println!("{}", file.file_stem().unwrap().to_str().unwrap());
-
         let file_name = file.file_stem().unwrap().to_str().unwrap();
-        let split: Vec<&str> = file_name.split(",").collect();
+        let split: Vec<&str> = file_name.split(',').collect();
 
         let x: i32 = split[0].parse().unwrap();
         let z: i32 = split[1].parse().unwrap();
 
         filename_and_numbers_vec.push(FilenameAndNumbers {
-            fileName: file.clone(),
+            file_name: file.clone(),
             x,
             z,
         });
@@ -97,8 +95,6 @@ fn main() {
         }
     }
 
-    // println!("{:?}", bounds);
-
     let xdiff = bounds.max_x - bounds.min_x + 1;
     let zdiff = bounds.max_z - bounds.min_z + 1;
 
@@ -112,16 +108,10 @@ fn main() {
     );
 
     for file_struc in &filename_and_numbers_vec {
-        let tile_img = image::open(&file_struc.fileName).unwrap();
+        let tile_img = image::open(&file_struc.file_name).unwrap();
 
-        let x_sector = (file_struc.x + -bounds.min_x);
-        let z_sector = (file_struc.z + -bounds.min_z);
-
-        // println!("file_struc.x: {}", file_struc.x);
-        // println!("file_struc.z: {}", file_struc.z);
-
-        // println!("x_sector: {}", x_sector);
-        // println!("z_sector: {}", z_sector);
+        let x_sector = file_struc.x + -bounds.min_x;
+        let z_sector = file_struc.z + -bounds.min_z;
 
         // for every pixel
         for x in 0..image_tile_width {
@@ -130,16 +120,8 @@ fn main() {
                 let pixel = tile_img.get_pixel(x.try_into().unwrap(), z.try_into().unwrap());
 
                 // calculate where pixel should go on output image
-
                 let output_pixel_x = x + (x_sector * image_tile_width);
-
                 let output_pixel_z = z + (z_sector * image_tile_height);
-
-                // let output_pixel_x = x - (-bounds.min_x * image_tile_width);
-                // let output_pixel_z = z - (-bounds.min_z * image_tile_height);
-
-                // println!("output_pixel_x: {}",output_pixel_x);
-                // println!("output_pixel_z: {}",output_pixel_z);
 
                 output_imgbuf.put_pixel(
                     output_pixel_x.try_into().unwrap(),
@@ -153,16 +135,28 @@ fn main() {
     // Write the contents of this image to the Writer in PNG format.
     output_imgbuf.save("./test.png").unwrap();
 
-    // // The dimensions method returns the images width and height.
-    // println!("dimensions {:?}", img.dimensions());
+    //>  shrink image a few times
+        let mut dynamic = DynamicImage::ImageRgba8(output_imgbuf);
 
-    // // The color method returns the image's `ColorType`.
-    // println!("{:?}", img.color());
+        dynamic = dynamic.resize(
+            dynamic.dimensions().0 / 2,
+            dynamic.dimensions().1 / 2,
+            FilterType::Lanczos3,
+        );
+        dynamic.save("./test2.png").unwrap();
 
-    // // let x_offset = (-bounds.max_x + bounds.min_x.abs()) -1;
+        dynamic = dynamic.resize(
+            dynamic.dimensions().0 / 2,
+            dynamic.dimensions().1 / 2,
+            FilterType::Lanczos3,
+        );
+        dynamic.save("./test3.png").unwrap();
 
-    // println!("x_offset: {}", -bounds.min_x * image_tile_width);
-
-    // positive Z is South.
-    // positive X is East
+        dynamic = dynamic.resize(
+            dynamic.dimensions().0 / 2,
+            dynamic.dimensions().1 / 2,
+            FilterType::Lanczos3,
+        );
+        dynamic.save("./test4.png").unwrap();
+    //<
 }
