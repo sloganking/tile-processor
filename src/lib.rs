53247609bcfdbb147dbf3a2de68c5b0b93c3f9f8
Logan King <seanloganking@gmail.com>
2022-06-08 15:06:50 -0400

    [WIP] shrink_tiles()

---
+++ src/lib.rs
@@ -0,0 +1,240 @@
+pub mod tiler {
+    use glob::{glob, GlobError};
+    use image::{GenericImageView, ImageBuffer, Rgba, RgbaImage, DynamicImage, imageops::FilterType};
+    use std::{path::PathBuf, collections::HashMap, fmt::format};
+
+    const IMAGE_TILE_WIDTH: i32 = 512;
+    const IMAGE_TILE_HEIGHT: i32 = 512;
+
+    #[derive(Debug)]
+    struct Bounds {
+        max_x: i32,
+        max_z: i32,
+        min_x: i32,
+        min_z: i32,
+    }
+
+    struct FilenameAndNumbers {
+        file_name: PathBuf,
+        x: i32,
+        z: i32,
+    }
+
+    /// Returns a list of all files in a directory and it's subdirectories
+    pub fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobError> {
+        //> get list of all files and dirs in path, using glob
+            let mut paths = Vec::new();
+
+            let mut potential_slash = "";
+            if PathBuf::from(path).is_dir() && !path.ends_with('/') {
+                potential_slash = "/";
+            }
+
+            let search_params = String::from(path) + potential_slash + "**/*" + filetype;
+
+            for entry in glob(&search_params).expect("Failed to read glob pattern") {
+                match entry {
+                    Ok(path) => {
+                        paths.push(path);
+                    }
+                    Err(e) => return Err(e),
+                }
+            }
+
+        //<> filter out directories
+            let paths = paths.into_iter().filter(|e| e.is_file());
+        //<
+
+        let paths: Vec<PathBuf> = paths.into_iter().collect();
+        Ok(paths)
+    }
+
+    pub fn consolidate_images(files: &[PathBuf]) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+        let mut bounds = Bounds {
+            max_x: i32::MIN,
+            max_z: i32::MIN,
+            min_x: i32::MAX,
+            min_z: i32::MAX,
+        };
+
+        let mut filename_and_numbers_vec: Vec<FilenameAndNumbers> = Vec::new();
+
+        // find max and min dimensions
+        for file in files {
+            let file_name = file.file_stem().unwrap().to_str().unwrap();
+            let split: Vec<&str> = file_name.split(',').collect();
+
+            let x: i32 = split[0].parse().unwrap();
+            let z: i32 = split[1].parse().unwrap();
+
+            filename_and_numbers_vec.push(FilenameAndNumbers {
+                file_name: file.clone(),
+                x,
+                z,
+            });
+
+            if x > bounds.max_x {
+                bounds.max_x = x;
+            }
+            if z > bounds.max_z {
+                bounds.max_z = z;
+            }
+            if x < bounds.min_x {
+                bounds.min_x = x;
+            }
+            if z < bounds.min_z {
+                bounds.min_z = z;
+            }
+        }
+
+        let xdiff = bounds.max_x - bounds.min_x + 1;
+        let zdiff = bounds.max_z - bounds.min_z + 1;
+
+        // Create a new ImgBuf with width: imgx and height: imgy
+        let mut output_imgbuf = RgbaImage::new(
+            (xdiff * IMAGE_TILE_WIDTH).try_into().unwrap(),
+            (zdiff * IMAGE_TILE_HEIGHT).try_into().unwrap(),
+        );
+
+        for file_struc in &filename_and_numbers_vec {
+            let tile_img = image::open(&file_struc.file_name).unwrap();
+
+            let x_sector = file_struc.x + -bounds.min_x;
+            let z_sector = file_struc.z + -bounds.min_z;
+
+            // for every pixel
+            for x in 0..IMAGE_TILE_WIDTH {
+                for z in 0..IMAGE_TILE_HEIGHT {
+                    // get pixel from tile image
+                    let pixel = tile_img.get_pixel(x.try_into().unwrap(), z.try_into().unwrap());
+
+                    // calculate where pixel should go on output image
+                    let output_pixel_x = x + (x_sector * IMAGE_TILE_WIDTH);
+                    let output_pixel_z = z + (z_sector * IMAGE_TILE_HEIGHT);
+
+                    output_imgbuf.put_pixel(
+                        output_pixel_x.try_into().unwrap(),
+                        output_pixel_z.try_into().unwrap(),
+                        pixel,
+                    )
+                }
+            }
+        }
+
+        output_imgbuf
+    }
+
+    fn insert_image(souce_image: ImageBuffer<Rgba<u8>, Vec<u8>>, inserted_image: ImageBuffer<Rgba<u8>, Vec<u8>>, x_offset: u32, y_offset: u32){
+
+    }
+
+    pub fn shrink_tiles(input_files: Vec<PathBuf>, output_dir: &str) {
+        let mut filenums_map = HashMap::new();
+
+        let mut rendered_output_tiles_map = HashMap::new();
+       
+        for file in &input_files {
+            //> fill filename_and_numbers_vec
+                let mut filename_and_numbers_vec: Vec<FilenameAndNumbers> = Vec::new();
+
+                let file_name = file.file_stem().unwrap().to_str().unwrap();
+                let split: Vec<&str> = file_name.split(',').collect();
+
+                let x: i32 = split[0].parse().unwrap();
+                let z: i32 = split[1].parse().unwrap();
+
+                filename_and_numbers_vec.push(FilenameAndNumbers {
+                    file_name: file.clone(),
+                    x,
+                    z,
+                });
+
+            //<> fill hashmap
+                filenums_map.insert((x,z), file);
+            //<
+        }
+        
+
+        for ((x, y), pathbuf) in &filenums_map{
+
+            // determine coords of output tile
+            let output_tile_x = if x%2 == 0{
+                *x
+            } else{
+                *x-1
+            };
+            let output_tile_y = if y%2 == 0{
+                *y
+            } else{
+                *y-1
+            };
+
+            //skip any already rendered tiles
+            if rendered_output_tiles_map.contains_key(&(output_tile_x, output_tile_y)){
+                continue;
+            }
+
+            // initialize output image
+            let mut output_imgbuf = RgbaImage::new(
+                (2 * IMAGE_TILE_WIDTH).try_into().unwrap(),
+                (2 * IMAGE_TILE_HEIGHT).try_into().unwrap(),
+            );
+
+            //> convert 4 images into one big image
+                // for the 4 sectors of the new tile
+                for x_sector in 0..=1{
+                    for y_sector in 0..=1{
+
+                        let real_x = output_tile_x + x_sector;
+                        let real_y = output_tile_y + y_sector;
+
+                        match filenums_map.get(&(real_x,real_y)){
+                            Some(path) => {
+                                let input_tile_img = image::open(&path).unwrap();
+
+                                // transfer image
+                                for x in 0..IMAGE_TILE_WIDTH{
+                                    for y in 0..IMAGE_TILE_HEIGHT{
+
+                                        // get pixel from tile image
+                                        let pixel = input_tile_img.get_pixel(x.try_into().unwrap(), y.try_into().unwrap());
+
+                                        // calculate where pixel should go on output image
+                                        let output_pixel_x = x + (x_sector * IMAGE_TILE_WIDTH);
+                                        let output_pixel_z = y + (y_sector * IMAGE_TILE_HEIGHT);
+
+                                        output_imgbuf.put_pixel(
+                                            output_pixel_x.try_into().unwrap(),
+                                            output_pixel_z.try_into().unwrap(),
+                                            pixel,
+                                        )
+
+                                    }
+                                }
+                            },
+                            None => continue,
+                        }
+                    }
+                }
+
+            //<> resize output image
+                let mut dynamic = DynamicImage::ImageRgba8(output_imgbuf);
+                dynamic = dynamic.resize(
+                    dynamic.dimensions().0 / 2,
+                    dynamic.dimensions().1 / 2,
+                    FilterType::Lanczos3,
+                );
+
+            //<> save file
+                let output_tile_filename = (output_tile_x/2).to_string() + "," + &(output_tile_y/2).to_string() + ".png";
+                println!("output_tile_filename: {}",output_tile_filename);
+                dynamic.save(output_dir.to_owned() + &output_tile_filename).expect("failed to save file");
+            //<
+            
+            // mark output image tile as rendered
+            rendered_output_tiles_map.insert((output_tile_x, output_tile_y), true);
+        }
+
+            
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,9 @@
 use colored::Colorize;
-use glob::{glob, GlobError};
-use image::{imageops::FilterType, DynamicImage, GenericImageView, RgbaImage};
-use std::path::PathBuf;
-
-/// Returns a list of all files in a directory and it's subdirectories
-pub fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobError> {
-    //> get list of all files and dirs in path, using glob
-        let mut paths = Vec::new();
-
-        let mut potential_slash = "";
-        if PathBuf::from(path).is_dir() && !path.ends_with('/') {
-            potential_slash = "/";
-        }
-
-        let search_params = String::from(path) + potential_slash + "**/*" + filetype;
-
-        for entry in glob(&search_params).expect("Failed to read glob pattern") {
-            match entry {
-                Ok(path) => {
-                    paths.push(path);
-                }
-                Err(e) => return Err(e),
-            }
-        }
-
-    //<> filter out directories
-        let paths = paths.into_iter().filter(|e| e.is_file());
-    //<
-
-    let paths: Vec<PathBuf> = paths.into_iter().collect();
-    Ok(paths)
-}
+use image::{imageops::FilterType, DynamicImage, GenericImageView};
+use map_combine::tiler::{consolidate_images, get_files_in_dir, shrink_tiles};
 
 fn print_err(err: &str) {
     println!("{}: {}", "error".red().bold(), err);
-}
-
-#[derive(Debug)]
-struct Bounds {
-    max_x: i32,
-    max_z: i32,
-    min_x: i32,
-    min_z: i32,
-}
-
-struct FilenameAndNumbers {
-    file_name: PathBuf,
-    x: i32,
-    z: i32,
 }
 
 fn main() {
@@ -58,105 +14,52 @@ fn main() {
         return;
     }
 
-    let mut bounds = Bounds {
-        max_x: i32::MIN,
-        max_z: i32::MIN,
-        min_x: i32::MAX,
-        min_z: i32::MAX,
-    };
-
-    let mut filename_and_numbers_vec: Vec<FilenameAndNumbers> = Vec::new();
-
-    // find max and min dimensions
-    for file in &files {
-        let file_name = file.file_stem().unwrap().to_str().unwrap();
-        let split: Vec<&str> = file_name.split(',').collect();
-
-        let x: i32 = split[0].parse().unwrap();
-        let z: i32 = split[1].parse().unwrap();
-
-        filename_and_numbers_vec.push(FilenameAndNumbers {
-            file_name: file.clone(),
-            x,
-            z,
-        });
-
-        if x > bounds.max_x {
-            bounds.max_x = x;
-        }
-        if z > bounds.max_z {
-            bounds.max_z = z;
-        }
-        if x < bounds.min_x {
-            bounds.min_x = x;
-        }
-        if z < bounds.min_z {
-            bounds.min_z = z;
-        }
-    }
-
-    let xdiff = bounds.max_x - bounds.min_x + 1;
-    let zdiff = bounds.max_z - bounds.min_z + 1;
-
-    let image_tile_width = 512;
-    let image_tile_height = 512;
-
-    // Create a new ImgBuf with width: imgx and height: imgy
-    let mut output_imgbuf = RgbaImage::new(
-        (xdiff * image_tile_width).try_into().unwrap(),
-        (zdiff * image_tile_height).try_into().unwrap(),
-    );
-
-    for file_struc in &filename_and_numbers_vec {
-        let tile_img = image::open(&file_struc.file_name).unwrap();
-
-        let x_sector = file_struc.x + -bounds.min_x;
-        let z_sector = file_struc.z + -bounds.min_z;
-
-        // for every pixel
-        for x in 0..image_tile_width {
-            for z in 0..image_tile_height {
-                // get pixel from tile image
-                let pixel = tile_img.get_pixel(x.try_into().unwrap(), z.try_into().unwrap());
-
-                // calculate where pixel should go on output image
-                let output_pixel_x = x + (x_sector * image_tile_width);
-                let output_pixel_z = z + (z_sector * image_tile_height);
-
-                output_imgbuf.put_pixel(
-                    output_pixel_x.try_into().unwrap(),
-                    output_pixel_z.try_into().unwrap(),
-                    pixel,
-                )
-            }
-        }
-    }
+    let output_imgbuf = consolidate_images(&files);
 
     // Write the contents of this image to the Writer in PNG format.
-    output_imgbuf.save("./test.png").unwrap();
+    output_imgbuf
+        .save("./test.png")
+        .expect("failed to save file");
 
-    //>  shrink image a few times
-        let mut dynamic = DynamicImage::ImageRgba8(output_imgbuf);
+    //> shrink image a few times
+        // let mut dynamic = DynamicImage::ImageRgba8(output_imgbuf);
 
-        dynamic = dynamic.resize(
-            dynamic.dimensions().0 / 2,
-            dynamic.dimensions().1 / 2,
-            FilterType::Lanczos3,
-        );
-        dynamic.save("./test2.png").unwrap();
+        // dynamic = dynamic.resize(
+        //     dynamic.dimensions().0 / 2,
+        //     dynamic.dimensions().1 / 2,
+        //     FilterType::Lanczos3,
+        // );
+        // dynamic.save("./test2.png").expect("failed to save file");
 
-        dynamic = dynamic.resize(
-            dynamic.dimensions().0 / 2,
-            dynamic.dimensions().1 / 2,
-            FilterType::Lanczos3,
-        );
-        dynamic.save("./test3.png").unwrap();
+        // dynamic = dynamic.resize(
+        //     dynamic.dimensions().0 / 2,
+        //     dynamic.dimensions().1 / 2,
+        //     FilterType::Lanczos3,
+        // );
+        // dynamic.save("./test3.png").expect("failed to save file");
 
-        dynamic = dynamic.resize(
-            dynamic.dimensions().0 / 2,
-            dynamic.dimensions().1 / 2,
-            FilterType::Lanczos3,
-        );
-        dynamic.save("./test4.png").unwrap();
+        // dynamic = dynamic.resize(
+        //     dynamic.dimensions().0 / 2,
+        //     dynamic.dimensions().1 / 2,
+        //     FilterType::Lanczos3,
+        // );
+        // dynamic.save("./test4.png").expect("failed to save file");
+    //<
+
+    //> shrink test
+        shrink_tiles(files, "./output_tiles/");
+
+        let files = get_files_in_dir("./output_tiles", "").unwrap();
+        if files.is_empty() {
+            print_err("no files found");
+            return;
+        }
+
+        let output_imgbuf = consolidate_images(&files);
+
+        // Write the contents of this image to the Writer in PNG format.
+        output_imgbuf
+            .save("./shrink_test.png")
+            .expect("failed to save file");
     //<
 }
