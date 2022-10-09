d48f56225b26122bf1e5a51b1a14cb0df4ef7cee
Logan King <seanloganking@gmail.com>
2022-10-08 21:57:43 -0400

    gen_tiles_to_dir()

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,39 @@ use colored::Colorize;
 use image::io::Reader;
 
 mod args;
-use args::TopSubcommands;
+use args::{GenTilesArgs, TopSubcommands};
 use map_combine::tiler::*;
 
 fn print_err(err: &str) {
     println!("{}: {}", "error".red().bold(), err);
     std::process::exit(1);
+}
+
+fn gen_tiles_to_dir(gen_tiles_args: GenTilesArgs) {
+    // get input image dimensions
+    let image_path = &gen_tiles_args.input.into_os_string().into_string().unwrap();
+    let dimensions = Reader::open(image_path).unwrap().into_dimensions().unwrap();
+
+    let output_dir = gen_tiles_args
+        .output
+        .into_os_string()
+        .into_string()
+        .unwrap();
+
+    println!("cleaning dir...");
+    clean_dir(&output_dir);
+
+    image_to_tiles(
+        image_path,
+        gen_tiles_args
+            .x_offset
+            .unwrap_or_else(|| (dimensions.0 / 2).try_into().unwrap()),
+        gen_tiles_args
+            .y_offset
+            .unwrap_or_else(|| (dimensions.1 / 2).try_into().unwrap()),
+        &output_dir,
+        gen_tiles_args.tile_dimensions,
+    );
 }
 
 fn main() {
@@ -17,57 +44,21 @@ fn main() {
 
     match args.top_commands {
         TopSubcommands::GenTiles(gen_tiles_args) => {
-            // get input image dimensions
-            let image_path = &gen_tiles_args.input.into_os_string().into_string().unwrap();
-            let dimensions = Reader::open(image_path).unwrap().into_dimensions().unwrap();
-
-            let output_dir = gen_tiles_args
-                .output
-                .into_os_string()
-                .into_string()
-                .unwrap();
-
-            println!("cleaning dir...");
-            clean_dir(&output_dir);
-
-            image_to_tiles(
-                image_path,
-                gen_tiles_args
-                    .x_offset
-                    .unwrap_or_else(|| (dimensions.0 / 2).try_into().unwrap()),
-                gen_tiles_args
-                    .y_offset
-                    .unwrap_or_else(|| (dimensions.1 / 2).try_into().unwrap()),
-                &output_dir,
-                gen_tiles_args.tile_dimensions,
-            );
+            gen_tiles_to_dir(gen_tiles_args);
         }
         TopSubcommands::GenTileLayers(gen_tiles_args) => {
-            // get input image dimensions
-            let image_path = &gen_tiles_args.input.into_os_string().into_string().unwrap();
-            let dimensions = Reader::open(image_path).unwrap().into_dimensions().unwrap();
-
             let output_dir = gen_tiles_args
                 .output
+                .clone()
                 .into_os_string()
                 .into_string()
                 .unwrap();
 
-            println!("cleaning dir...");
             clean_dir(&output_dir);
 
-            println!("slicing tiles...");
-            image_to_tiles(
-                image_path,
-                gen_tiles_args
-                    .x_offset
-                    .unwrap_or_else(|| (dimensions.0 / 2).try_into().unwrap()),
-                gen_tiles_args
-                    .y_offset
-                    .unwrap_or_else(|| (dimensions.1 / 2).try_into().unwrap()),
-                &(output_dir.clone() + "0/"),
-                gen_tiles_args.tile_dimensions,
-            );
+            let mut gen_tiles_args = gen_tiles_args;
+            gen_tiles_args.output.push("0/");
+            gen_tiles_to_dir(gen_tiles_args);
 
             generate_lods(&output_dir);
         }
